870faba0839e19993ecc920e5cfe19fe925a08e4
Emily81926 <emily81926@gmail.com>
2021-09-20 12:20:38 +0800

    feat: connect todo and user model

---
+++ models/todo.js
@@ -0,0 +1,15 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  const Todo = sequelize.define('Todo', {
+    name: DataTypes.STRING,
+    isDone: DataTypes.BOOLEAN
+  }, {})
+  Todo.associate = function (models) {
+    // associations can be defined here
+    Todo.belongsTo(models.User)
+  }
+  return Todo;
+};
